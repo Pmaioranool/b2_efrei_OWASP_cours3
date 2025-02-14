d2db58431a4bd4dd5b89fdd269f169f0876c2a7c
Drx93 <dorian93.roux@gmail.com>
2025-02-14 01:08:52 +0100

    Add html

--- templates/register.php
+++ templates/register.php
@@ -1,6 +1,10 @@
 <main id="log_sign">
-        <h1>Connectez-vous</h1>
+        <h1>Inscrivez-vous</h1>
         <form action="">
+            <label for="name">Nom</label>
+            <input type="text" maxlength="100" name="name" id="name" placeholder="Entrez votre nom">
+            <label for="firstname">Prénom</label>
+            <input type="text" maxlength="100" name="firstname" id="firstname" placeholder="Entrez votre Prenom">
             <label for="mail">Adresse mail</label>
             <input type="mail" maxlength="200" name="mail" id="mail" placeholder="Entrez votre mail">
             <label for="mdp">Mot de passe</label>
